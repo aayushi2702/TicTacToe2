e0ec6167d00b6b28befee86fa8a976d968118e15
Ayushi <aayushijain2702@gmail.com>
2022-12-27 17:27:25 +0530

    feat : Player X should move first to start a game.

---
+++ TicTacToe2/src/main/java/com/tictactoe/game/controller/GameController.java
@@ -0,0 +1,25 @@
+package com.tictactoe.game.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tictactoe.game.service.GameService;
+
+@RestController
+public class GameController {
+
+	private final GameService gameService;
+
+	public GameController(GameService gameService) {
+		this.gameService = gameService;
+	}
+
+	@PostMapping(value = "/tictactoe/play/{player}")
+	public ResponseEntity<String> playGameHandler(@PathVariable(name = "player") char player) {
+		return ResponseEntity.status(HttpStatus.OK).body(gameService.playGame(player));
+	}
+
+}
