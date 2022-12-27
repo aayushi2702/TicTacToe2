f1f742ce3f3770d85a81679e763169bbe04d96a3
Ayushi <aayushijain2702@gmail.com>
2022-12-27 15:13:23 +0530

    build : Implementing Swagger API.

--- TicTacToe2/src/main/java/com/tictactoe/game/TicTacToeApplication.java
+++ TicTacToe2/src/main/java/com/tictactoe/game/TicTacToeApplication.java
@@ -3,7 +3,10 @@ package com.tictactoe.game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class TicTacToeApplication {
 
 	public static void main(String[] args) {
