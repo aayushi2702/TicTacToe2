291bebabbff24ae691dd943443b8697d46011fd0
Ayushi <aayushijain2702@gmail.com>
2022-12-27 15:29:07 +0530

    chore : Adding custom documentation in Swagger API.

--- TicTacToe2/src/main/java/com/tictactoe/game/TicTacToeApplication.java
+++ TicTacToe2/src/main/java/com/tictactoe/game/TicTacToeApplication.java
@@ -1,8 +1,15 @@
 package com.tictactoe.game;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,4 +20,15 @@ public class TicTacToeApplication {
 		SpringApplication.run(TicTacToeApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
+	}
+
+	public static final Contact DEFAULT_CONTACT = new Contact("Ayushi", "http://localhost:8082/",
+			"aayushij2702@gmail.com");
+
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Tic Tac Toe Application",
+			"Tic Tac Toe API Documentation", "1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0",
+			"http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
 }
