94ce62671d50c4fcc7fc356309e2e0fe97c11e5e
Ayushi <aayushijain2702@gmail.com>
2022-12-28 17:52:44 +0530

    feat : Declare tie if all positions on game board are occupied

--- TicTacToe2/src/test/java/com/tictactoe/game/service/GameServiceTests.java
+++ TicTacToe2/src/test/java/com/tictactoe/game/service/GameServiceTests.java
@@ -86,4 +86,17 @@ public class GameServiceTests {
 		gameService.playGame(Player.O, Position.SIX.getValue());
 		assertThat(gameService.playGame(Player.X, Position.NINE.getValue()).getStatus()).isEqualTo("GAME_OVER");
 	}
+
+	@Test
+	public void shouldDeclareTieIfAllPositionsAreFilledWithoutAnyPlayerFillingConsecutiveRowsColumnsOrDiagonals() {
+		gameService.playGame(Player.X, Position.ONE.getValue());
+		gameService.playGame(Player.O, Position.THREE.getValue());
+		gameService.playGame(Player.X, Position.TWO.getValue());
+		gameService.playGame(Player.O, Position.FIVE.getValue());
+		gameService.playGame(Player.X, Position.SEVEN.getValue());
+		gameService.playGame(Player.O, Position.EIGHT.getValue());
+		gameService.playGame(Player.X, Position.NINE.getValue());
+		gameService.playGame(Player.O, Position.FOUR.getValue());
+		assertThat(gameService.playGame(Player.X, Position.SIX.getValue()).getResult()).isEqualTo("Game is a Tie");
+	}
 }
