e613e27892b0f31eb2d673e616e16bbefd0b7989
Ayushi <aayushijain2702@gmail.com>
2022-12-28 12:28:37 +0530

    Refactor: Extract game board initialization to separate method in test-cases class

--- TicTacToe2/src/test/java/com/tictactoe/game/service/impl/GameBoardTests.java
+++ TicTacToe2/src/test/java/com/tictactoe/game/service/impl/GameBoardTests.java
@@ -2,6 +2,7 @@ package com.tictactoe.game.service.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -11,9 +12,15 @@ import com.tictactoe.game.enums.Player;
 @RunWith(MockitoJUnitRunner.class)
 public class GameBoardTests {
 
+	private GameBoard gameBoard;
+
+	@Before
+	public void setUp() {
+		gameBoard = new GameBoard();
+	}
+
 	@Test
 	public void shouldSaveInputValueOnGameBoard() {
-		GameBoard gameBoard = new GameBoard();
 		gameBoard.setPlayerInPosition(0, 1, Player.X);
 		assertThat(gameBoard.getPlayerInPosition(0, 1)).isEqualTo(Player.X.getValue());
 	}
